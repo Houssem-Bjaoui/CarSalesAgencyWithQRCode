0b1876330e5180567f1dd797de69b84990f1a486
Houssem_Bejaoui <houssem.bjaoui00@gmail.com>
2025-02-16 11:04:12 +0100

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/example/CarSalesAgency/CarSalesAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarSalesAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalesAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSalesAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarSalesAgency/CarSalesAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarSalesAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSalesAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
